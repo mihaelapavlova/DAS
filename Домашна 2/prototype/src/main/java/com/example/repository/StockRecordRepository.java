a07f396662cbca11219192dbf75454a1a36d74a7
Mihaela Pavlova <121287782+mihaelapavlova@users.noreply.github.com>
2024-12-04 21:31:09 +0100

    Update StockRecordRepository.java

--- Домашна 2/prototype/src/main/java/com/example/repository/StockRecordRepository.java
+++ Домашна 2/prototype/src/main/java/com/example/repository/StockRecordRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StockRecordRepository extends JpaRepository<StockRecord, Long> {
-    List<StockRecord> findByStockCode(String stockCode);  // Пребарување записи според stockCode
+    List<StockRecord> findByStockCode(String stockCode);  
 }
